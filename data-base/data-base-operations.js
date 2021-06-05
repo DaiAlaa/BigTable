105bdb872c199c388903ad536ea96af017faf96a
kareem3m <kareem.mohamed9711@gmail.com>
2021-06-05 17:05:57 +0200

    integration 1

--- data-base/data-base-operations.js
+++ data-base/data-base-operations.js
@@ -1,7 +1,47 @@
 const Course = require('./data-base-schema');
+const { CourseA: CourseA } = require("./data-base-schema");
+const { CourseB: CourseB } = require("./data-base-schema");
+const { CourseMaster: CourseMaster } = require("./data-base-schema");
+const { MetadataTabletServer1: MetadataTabletServer1 } = require("./data-base-schema");
 
-//string
+async function DeleteRow(url, num) {
+    if (num == 1) return await CourseA.deleteMany({ url: url });
+    else if(num == 2) return await CourseB.deleteMany({ url: url });
+    else if(num == 3) return await Course.deleteMany({ url: url });
+    else return await CourseMaster.deleteMany({ url: url });
+}
+
+  async function AddRow(data, num) {
+    try {
+      if (num == 1) return await CourseA.insertMany(data);
+      else if(num == 2) return await CourseB.insertMany(data);
+      else if(num == 3) return await Course.insertMany(data);
+      else return await CourseMaster.insertMany(data);
+
+    } catch (e) {}
+  }
+  async function ReadRows(url, num) {
+    if (num == 1) return await CourseA.find({ url: url }, {}, {});
+    else if(num == 2)  return await CourseB.find({ url: url }, {}, {});
+    else if(num == 3)  return await CourseB.find({ url: url }, {}, {});
+    else return await CourseMaster.find({ url: url }, {}, {});
+}
+  async function set(url, data, num) {
+    if (num == 1) return await CourseA.updateMany({ url: url }, { $set: data });
+    else if(num == 2)  return await CourseB.updateMany({ url: url }, { $set: data });
+    else if(num == 3) return await Course.updateMany({ url: url }, { $set: data });
+    else return await CourseMaster.updateMany({ url: url }, { $set: data });
+
+  }
+  async function DeleteCells(url, data, num) {
+    if (num == 1) return await CourseA.updateMany({ url: url }, { $unset: data });
+    else if(num == 2)  return await CourseB.updateMany({ url: url }, { $unset: data });
+    else if(num == 3)  return await Course.updateMany({ url: url }, { $unset: data });
+    else  return await CourseMaster.updateMany({ url: url }, { $unset: data });
+  
+
+}
+
+  
  
- module.exports= {Set, DeleteCells, DeleteRow, ReadRows, AddRow}
- 
- se
+ module.exports= {set, DeleteCells, DeleteRow, ReadRows, AddRow}
